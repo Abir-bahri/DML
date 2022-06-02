d7507666c51db6714ec81838b66904dcd8e1982d
Abir-bahri <bahriabir15@gmail.com>
2022-06-03 00:07:46 +0200

    first commit

---
+++ DML.sql
@@ -0,0 +1,11 @@
+Insert Into Product Values ( 'P01' , 'Samsung Galaxy S20', 'SmartPhone', 3299),
+
+Insert Into Product Values ( 'P02' , 'Asus NoteBook', 'PC', 4599),
+
+Insert Into Customers Values ( 'C01' , 'ALI', 71321009),
+
+Insert Into Customers Values ( 'C02' , 'ASMA', 77345823),
+
+Insert Into Orders Values ( 'C01' , 'P01' ,NULL, 2, 9198),
+
+Insert Into Orders Values ( 'C02' , 'P02' ,28/05/2020, 1, 3299)
